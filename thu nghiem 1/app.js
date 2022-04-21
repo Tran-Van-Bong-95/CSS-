dca76d966d6a9277fd188c43210e5626e696e130
Tran-Van-Bong-95 <uchihaitachi888813@gmail.com>
2022-04-22 06:33:14 +0700

    thu_nghiem_git_hub

---
+++ thu nghiem 1/app.js
@@ -0,0 +1,10 @@
+const sidebar = document.querySelector('.sidebar')
+console.log(sidebar)
+const toggle = document.querySelector('.toggle')
+toggle.addEventListener('click', () => {
+  sidebar.classList.add('show')
+})
+const close_sidebar = document.querySelector('.sidebar button')
+close_sidebar.addEventListener('click', () => {
+  sidebar.classList.remove('show')
+})
